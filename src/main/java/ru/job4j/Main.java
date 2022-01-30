5e966b78d7c70770b678416b69dba71eff9745eb
olegzhigalov5 <o010165@ya.ru>
2022-01-30 19:36:56 +0300

    3. Арифметические операции. [#185]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -1,7 +1,17 @@
 package ru.job4j;
 
 public class Main {
+  int two = 2;
+  int six = 6;
+  int four = 4;
+  int five = 5;
+  int sixDivTwo = six / two;
+  int fiveMinusTwo = five - two;
+  int fourTimeTwo = four * two;
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+      System.out.println("sixDivTwo");
+      System.out.println("fiveMinusTwo");
+      System.out.println("fourTimeTwo");
     }
 }
