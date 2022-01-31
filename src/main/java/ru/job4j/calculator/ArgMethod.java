ad8bedf0ddc0e01e00148cbad7de0af38cee015c
olegzhigalov5 <o010165@ya.ru>
2022-01-31 16:53:47 +0300

    4.1. Аргументы [#295521]

--- src/main/java/ru/job4j/calculator/ArgMethod.java
+++ src/main/java/ru/job4j/calculator/ArgMethod.java
@@ -15,20 +15,3 @@ public class ArgMethod {
         ArgMethod.hello(name, age);
     }
 }
-//package ru.job4j.calculator;
-
-//public class Calculator {
-
-    //public static void main(String[] args) {
-      //  int two = 2;
-        //int six = 6;
-        //int four = 4;
-        //int five = 5;
-        //int sixDivTwo = six / two;
-        //int fiveMinusTwo = five - two;
-        //int fourTimeTwo = four * two;
-        //System.out.println(sixDivTwo);
-        //System.out.println(fiveMinusTwo);
-        //System.out.println(fourTimeTwo);
-    //}
-//}
