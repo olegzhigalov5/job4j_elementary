264684479953df5fcba9a568dfb07059fcef7c09
olegzhigalov5 <o010165@ya.ru>
2022-02-02 14:18:01 +0300

    1. Модульные тесты. [#159435]

---
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.converter;
+
+import org.junit.*;
+
+public class ConverterTest {
+
+    @Test
+    public void rubleToEuro() {
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
+    }
+
+    @Test
+    public void rubleToDollar() {
+        float in = 140;
+        float expected = 2.3333f;
+        float out = Converter.rubleToDollar(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
+    }
+}
