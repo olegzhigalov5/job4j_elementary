b927e74c2a2693d09ab64b134f5cd3fe481078b2
olegzhigalov5 <o010165@ya.ru>
2022-02-06 20:53:03 +0300

    4.5. Отладка программы в IDEA [#159464]

---
+++ src/test/java/ru/job4j/condition/ThreeMaxTest.java
@@ -0,0 +1,96 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ThreeMaxTest {
+    @Test
+    public void firstMax() {
+        int first = 10;
+        int second = 5;
+        int third = 1;
+        int result = ThreeMax.max(first, second, third);
+        int expected = 10;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void secondMax() {
+        int first = 10;
+        int second = 50;
+        int third = 1;
+        int result = ThreeMax.max(first, second, third);
+        int expected = 50;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void thirdMax() {
+        int first = 1;
+        int second = 5;
+        int third = 100;
+        int result = ThreeMax.max(first, second, third);
+        int expected = 100;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void allEq() {
+        int first = 1;
+        int second = 1;
+        int third = 1;
+        int result = ThreeMax.max(first, second, third);
+        int expected = 1;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void firstEqSecond() {
+        int first = 10;
+        int second = 10;
+        int third = 1;
+        int result = ThreeMax.max(first, second, third);
+        int expected = 10;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void firstEqThird() {
+        int first = 100;
+        int second = 1;
+        int third = 100;
+        int result = ThreeMax.max(first, second, third);
+        int expected = 100;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void secondEqThird() {
+        int first = 1;
+        int second = 100;
+        int third = 100;
+        int result = ThreeMax.max(first, second, third);
+        int expected = 100;
+        Assert.assertEquals(expected, result);
+    }
+
+    public static class TriangleTest {
+        @Test
+        public void whenExist() {
+            double ab = 2.0;
+            double ac = 2.0;
+            double bc = 2.0;
+            boolean result = Triangle.exist(ab, ac, bc);
+            Assert.assertTrue(result);
+        }
+
+        @Test
+        public void whenNoExist() {
+            double ab = 2.0;
+            double ac = 2.0;
+            double bc = 6.0;
+            boolean result = Triangle.exist(ab, ac, bc);
+            Assert.assertFalse(result);
+        }
+    }
+}
