c308a927eb5539be13615353c0c3c19e64d0fa76
olegzhigalov5 <o010165@ya.ru>
2022-02-14 22:37:02 +0300

    6.5.3. Сортировка выборкой. [#156320]

--- src/main/java/ru/job4j/array/SwitchArray.java
+++ src/main/java/ru/job4j/array/SwitchArray.java
@@ -8,12 +8,4 @@ public class SwitchArray {
         array[dest] = temp;
         return array;
     }
-
-    /*public static void main(String[] args) {
-        int[] nums = new int[]{1, 2, 3, 4, 5, 6};
-        int[] rsl = swapBorder(nums);
-        for (int index : nums) {
-            System.out.println(index);
-        }
-    }*/
 }
